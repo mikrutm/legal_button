43c13d14e7940c83c49e6807c4d33d1bc3bbce52
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-23 15:21:31 +0200

    srew

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
-
+import pandas as pd    
 with st.echo():
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
     from webdriver_manager.core.os_manager import ChromeType
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait 
+    from selenium.webdriver.support import expected_conditions as EC
 
     @st.cache_resource
     def get_driver():
@@ -22,5 +25,45 @@ with st.echo():
 
     driver = get_driver()
     driver.get("https://www.gov.pl/web/premier/wplip-rm")
+
+
+
+    def extract_table():
+        # Znajdź tabelę na stronie
+        table = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, 'table'))
+        )
+
+        # Pobierz wszystkie wiersze i komórki z tabeli
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        table_data = []
+        for row in rows:
+            cols = row.find_elements(By.TAG_NAME, 'td')
+            cols = [col.text for col in cols]
+            table_data.append(cols)
+
+        return pd.DataFrame(table_data)
+
+        # Pobierz pierwszą tabelę
+        df1 = extract_table()
+
+        # Kliknij przycisk, aby załadować nową tabelę
+        button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="js-pagination-page-next"]'))
+        )
+        button.click()
+
+        # Poczekaj na załadowanie nowej tabeli
+        df2 = extract_table()
+
+        # Połącz tabele
+        df_combined = pd.concat([df1, df2], ignore_index=True)
+
+        # Zapisz wynik do pliku CSV
+        df_combined.to_csv('combined_table.csv', index=False)
+
+        # Zamknij przeglądarkę
+        driver.quit()
+
 
     st.code(driver.page_source)
