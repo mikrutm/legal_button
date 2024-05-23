734cb779850d738e0f5c7445283a6fd4842aee7b
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-23 17:06:37 +0200

    sadasd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+import pandas as pd
+from bs4 import BeautifulSoup
 
 """
 ## Web scraping on Streamlit Cloud with Selenium
@@ -10,28 +17,33 @@ This is a minimal, reproducible example of how to scrape the web with Selenium a
 Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
 """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
-
-    @st.cache_resource
-    def get_driver():
-        
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
-
+@st.cache(allow_output_mutation=True)
+def get_driver():        
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
 
+def extract_table_to_df(url):
     driver = get_driver()
-    driver.get("http://example.com")
+    driver.get(url)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    table = soup.find('table')
+    if table is None:
+        st.write("Nie znaleziono tabeli na stronie.")
+        return None
+    else:
+        return pd.read_html(str(table))[0]
 
-    st.code(driver.page_source)
+# Przykładowe użycie funkcji
+url = "https://www.gov.pl/web/premier/wplip-rm"  # Zastąp 'http://example.com' adresem twojej strony
+df = extract_table_to_df(url)
+if df is not None:
+    st.write("Tabela ze strony:")
+    st.write(df)
