032dfe82974567b64438a83e8e904ba45898dd9e
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-23 15:01:26 +0200

    erwe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,5 @@
 import streamlit as st
 
-"""
-## Web scraping on Streamlit Cloud with Selenium
-
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
-
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
-
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
-
-
 with st.echo():
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
@@ -32,6 +21,6 @@ with st.echo():
     options.add_argument("--headless")
 
     driver = get_driver()
-    driver.get("http://example.com")
+    driver.get("https://www.gov.pl/web/premier/wplip-rm")
 
     st.code(driver.page_source)
