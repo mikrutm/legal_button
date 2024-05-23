4602dd86a2416779e6fd5d06cfc64ee51f24f0fa
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-23 15:40:14 +0200

    ggg

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ with st.echo():
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait 
     from selenium.webdriver.support import expected_conditions as EC
+    from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 
     @st.cache_resource
     def get_driver():
