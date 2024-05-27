ef23937dd4d63fa5dfb8c9c8cdf2d32637ba465c
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-27 13:17:03 +0200

    asdas

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 @st.cache_resource
 def get_driver():
     options = Options()
