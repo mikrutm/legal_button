1494da88a35b236c2191a7d36aada9a8adb7ac00
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-27 12:38:26 +0200

    dfgre

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ def get_driver():
 options = Options()
 options.add_argument("--disable-gpu")
 options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
 driver = get_driver()
 driver.get("http://example.com")
