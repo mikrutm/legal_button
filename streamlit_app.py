6762fbec0dac9675aadf57349940d054fc7ed0b0
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-27 18:16:49 +0200

    sdsdfsdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ st.title("DORA - Wykaz Prac")
 st.write("Narzędzie pozwalające pobrać i przeglądać tabelę z wykazem prac legislacyjnych i programowych Rady Ministrów. To doraźna próba naprawienia błędu na stronie https://www.gov.pl/web/premier/wplip-rm. Jeśli ta aplikacja się popsuje, jest szansa że serwis gov uznaje, że jesteśmy złośliwym botem i zblokuje ściaganie danych. Radzę wtedy po prostu poczekać chwilę, lub porposić współpracownika o tej aplikacji na swoim komputererze.")
 st.write(" Tabelka się tworzy, proszę o cierpliwość.")
 
-
 def find_free_port():
     while True:
         port = random.randint(1024, 65535)
@@ -39,12 +38,6 @@ def find_free_host():
             print(host)
             return host
     raise RuntimeError("Nie znaleziono wolnych hostów w podanym zakresie.")
-
-
-
-host = find_free_host()
-port = find_free_port() 
-
 
 @st.cache_resource
 
@@ -79,94 +72,102 @@ def to_excel(df):
     processed_data = output.getvalue()
     return processed_data
 # Inicjalizacja WebDriver
-try:
-    driver = get_driver()
-    driver.get("https://www.gov.pl/web/premier/wplip-rm")
-except Exception as e:
-    st.error(f"Błąd podczas łączenia się z ChromeDriver: {e}")
-    driver.quit()
-    st.stop()
+
+
+if st.button("Aktualizuj tabelę"):
+
+    host = find_free_host()
+    port = find_free_port() 
+
+
+    try:
+        driver = get_driver()
+        driver.get("https://www.gov.pl/web/premier/wplip-rm")
+    except Exception as e:
+        st.error(f"Błąd podczas łączenia się z ChromeDriver: {e}")
+        driver.quit()
+        st.stop()
 
 # Kliknięcie przycisku ciasteczek
-try:
-    cookie_button = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/button"))
-    )
-    cookie_button.click()
-except Exception as e:
-    st.write("Przycisk ciasteczek nie został znaleziony lub już zaakceptowany.")
-    st.write(f"Błąd: {e}")
-
-try:
-    c_button = WebDriverWait(driver, 1).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/main/div/article/div[2]/div[2]/nav/div[2]/div/a[3]"))
-    )
-    c_button.click()
-except Exception as e:
-    st.write("Przycisk ciasteczek nie został znaleziony lub już zaakceptowany.")
-    st.write(f"Błąd: {e}")
-
-
-# Pobranie i łączenie danych z tabeli
-all_data = []
-
-try:
-    while True:
-        # Poczekaj na załadowanie tabeli
-        table_element = WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "results-table"))
+    try:
+        cookie_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/button"))
         )
-        table_html = table_element.get_attribute('outerHTML')
-        df = pd.read_html(table_html)[0]
-        print(df)
-        # Wyodrębnienie linków z każdej klasy "results-row"
-        rows = driver.find_elements(By.CLASS_NAME, "result-row")
-        print(rows)
-        row_links = [row.find_element(By.TAG_NAME, "a").get_attribute('href') for row in rows]
-        print(row_links)
+        cookie_button.click()
+    except Exception as e:
+        st.write("Przycisk ciasteczek nie został znaleziony lub już zaakceptowany.")
+        st.write(f"Błąd: {e}")
 
-        # Dodanie nowej kolumny z linkami do DataFrame
-        df['Link'] = row_links
-
-        all_data.append(df)
-      
-
-        # Próbuj kliknąć przycisk "Następna strona"
-        try:
-            next_button = driver.find_element(By.XPATH, '//*[@id="js-pagination-page-next"]')
-            if 'disabled' in next_button.get_attribute('class'):
-                break  # Wyjdź z pętli, jeśli przycisk jest nieaktywny
-            next_button.click()
-            time.sleep(2)  # Poczekaj na załadowanie następnej strony
-        except Exception as e:
-            st.write("Brak przycisku 'Następna strona' lub nieaktywny.")
-            st.write(f"Błąd: {e}")
-            break
-
-    # Połącz wszystkie zebrane dane w jeden DataFrame
-    driver.quit()    
-    final_df = pd.concat(all_data, ignore_index=True)
-    final_df.drop("Podgląd",axis=1,inplace=True)
-
-    st.dataframe(final_df)
-    import io
-  # Konwertuj DataFrame do formatu Excel i zapisz w pamięci
-    excel_buffer = io.BytesIO()
-    with pd.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
-        final_df.to_excel(writer, index=False)
-
-    # Przejdź do początku bufora
-    excel_buffer.seek(0)
-
-    # Dodaj przycisk do pobrania pliku
-    st.download_button(
-        label="Pobierz plik XLSX",
-        data=excel_buffer,
-        file_name="downloaded_file.xlsx",
-        mime="application/vnd.openxmlformats-officedocument.spreads")
+    try:
+        c_button = WebDriverWait(driver, 1).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/main/div/article/div[2]/div[2]/nav/div[2]/div/a[3]"))
+        )
+        c_button.click()
+    except Exception as e:
+        st.write("Przycisk ciasteczek nie został znaleziony lub już zaakceptowany.")
+        st.write(f"Błąd: {e}")
 
 
-except Exception as e:
-    st.write("Tabela nie została znaleziona lub nie mogła zostać załadowana.")
-    st.write(f"Błąd: {e}")
-st.write("Opracował: Andrzej Józefczyk")
+    # Pobranie i łączenie danych z tabeli
+    all_data = []
+
+    try:
+        while True:
+            # Poczekaj na załadowanie tabeli
+            table_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "results-table"))
+            )
+            table_html = table_element.get_attribute('outerHTML')
+            df = pd.read_html(table_html)[0]
+            print(df)
+            # Wyodrębnienie linków z każdej klasy "results-row"
+            rows = driver.find_elements(By.CLASS_NAME, "result-row")
+            print(rows)
+            row_links = [row.find_element(By.TAG_NAME, "a").get_attribute('href') for row in rows]
+            print(row_links)
+
+            # Dodanie nowej kolumny z linkami do DataFrame
+            df['Link'] = row_links
+
+            all_data.append(df)
+
+
+            # Próbuj kliknąć przycisk "Następna strona"
+            try:
+                next_button = driver.find_element(By.XPATH, '//*[@id="js-pagination-page-next"]')
+                if 'disabled' in next_button.get_attribute('class'):
+                    break  # Wyjdź z pętli, jeśli przycisk jest nieaktywny
+                next_button.click()
+                time.sleep(2)  # Poczekaj na załadowanie następnej strony
+            except Exception as e:
+                st.write("Brak przycisku 'Następna strona' lub nieaktywny.")
+                st.write(f"Błąd: {e}")
+                break
+
+        # Połącz wszystkie zebrane dane w jeden DataFrame
+        driver.quit()    
+        final_df = pd.concat(all_data, ignore_index=True)
+        final_df.drop("Podgląd",axis=1,inplace=True)
+
+        st.dataframe(final_df)
+        import io
+      # Konwertuj DataFrame do formatu Excel i zapisz w pamięci
+        excel_buffer = io.BytesIO()
+        with pd.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
+            final_df.to_excel(writer, index=False)
+
+        # Przejdź do początku bufora
+        excel_buffer.seek(0)
+
+        # Dodaj przycisk do pobrania pliku
+        st.download_button(
+            label="Pobierz plik XLSX",
+            data=excel_buffer,
+            file_name="downloaded_file.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreads")
+
+
+    except Exception as e:
+        st.write("Tabela nie została znaleziona lub nie mogła zostać załadowana.")
+        st.write(f"Błąd: {e}")
+    st.write("Opracował: Andrzej Józefczyk")
